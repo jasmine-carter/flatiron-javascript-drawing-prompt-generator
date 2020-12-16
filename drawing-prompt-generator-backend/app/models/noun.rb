4d3a27163012b4878348db819c2bc92a09809e6d
Jasmine Carter <heyjasminec@gmail.com>
2020-12-15 21:28:22 -0500

    Add Faker gem, build class method to randomly generate array of nouns, build class method to build random noun on prompt

--- drawing-prompt-generator-backend/app/models/noun.rb
+++ drawing-prompt-generator-backend/app/models/noun.rb
@@ -1,3 +1,24 @@
 class Noun < ApplicationRecord
   belongs_to :prompt
+
+  def self.random_noun(prompt)
+    value = Noun.randomizer.sample
+    Noun.create(value: value, prompt: prompt.id)
+  end
+
+
+  def self.randomizer
+    options = []
+    options << Faker::Games::SuperMario.character
+    options << Faker::Games::Zelda.character
+    options << Faker::Games::Pokemon.name
+    options << Faker::Movies::StarWars.character
+    options << Faker::TvShows::Simpsons.character
+    options << Faker::JapaneseMedia::DragonBall.character
+    options << Faker::Movies::LordOfTheRings.character
+    options << Faker::Creature::Animal.name
+    options << Faker::Creature::Animal.name
+    options << Faker::Creature::Animal.name
+  end
+
 end
