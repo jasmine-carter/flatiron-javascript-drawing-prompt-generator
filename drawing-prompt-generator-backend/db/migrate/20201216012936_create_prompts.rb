3e4a62272e552b7e8c6bdb2888a87ad00f7c845d
Jasmine Carter <heyjasminec@gmail.com>
2020-12-16 19:50:33 -0500

    Nuked old database and migrations

--- drawing-prompt-generator-backend/db/migrate/20201216012936_create_prompts.rb
+++
@@ -1,9 +0,0 @@
-class CreatePrompts < ActiveRecord::Migration[6.1]
-  def change
-    create_table :prompts do |t|
-      t.string :descriptor
-
-      t.timestamps
-    end
-  end
-end
--- drawing-prompt-generator-backend/db/migrate/20201216024953_create_descriptions.rb
+++
@@ -1,9 +0,0 @@
-class CreateDescriptions < ActiveRecord::Migration[6.1]
-  def change
-    create_table :descriptions do |t|
-      t.references :prompt, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
