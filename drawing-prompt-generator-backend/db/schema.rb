d89a921504ff4e24167204be7d350d03467f17e9
Jasmine Carter <heyjasminec@gmail.com>
2020-12-16 19:44:19 -0500

    Create Styles table and update db

--- drawing-prompt-generator-backend/db/schema.rb
+++ drawing-prompt-generator-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_004219) do
+ActiveRecord::Schema.define(version: 2020_12_17_004314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,12 @@ ActiveRecord::Schema.define(version: 2020_12_17_004219) do
     t.integer "verb_id"
     t.integer "prompt_id"
     t.integer "adjective_id"
+  end
+
+  create_table "styles", force: :cascade do |t|
+    t.string "value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "verbs", force: :cascade do |t|
