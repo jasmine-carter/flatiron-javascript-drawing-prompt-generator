02bd0485f76b6aa921bca09cf8236c298a51cf49
Jasmine Carter <heyjasminec@gmail.com>
2020-12-16 21:08:34 -0500

    Create class method to select random adjective

--- drawing-prompt-generator-backend/app/models/adjective.rb
+++ drawing-prompt-generator-backend/app/models/adjective.rb
@@ -1,4 +1,11 @@
 class Adjective < ApplicationRecord
   has_many :sentences
   has_many :prompts, through: :sentences
+  validates :value, uniqueness: true
+
+
+  def self.random_adjective
+    Adjective.all.sample.value
+  end
+
 end
