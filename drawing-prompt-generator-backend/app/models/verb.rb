5a7de06b8b72d0caf0789c58b369987e451a63d5
Jasmine Carter <heyjasminec@gmail.com>
2020-12-16 20:53:49 -0500

    Create randomizer class method for Verb generation

--- drawing-prompt-generator-backend/app/models/verb.rb
+++ drawing-prompt-generator-backend/app/models/verb.rb
@@ -1,4 +1,10 @@
 class Verb < ApplicationRecord
   has_many :sentences
   has_many :prompts, through: :sentences
+
+  def self.random_verb
+    value = Faker::Verb.ing_form
+    Verb.create(value: value)
+  end
+  
 end
