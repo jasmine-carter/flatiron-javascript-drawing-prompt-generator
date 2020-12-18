d0bce6f6e3ee3e79be467a62da87c558b7d577d5
Jasmine Carter <heyjasminec@gmail.com>
2020-12-17 21:08:03 -0500

    Link index.html file to src

---
+++ drawing-prompt-generator-frontend/src/index.js
@@ -0,0 +1,5 @@
+
+//design button to generate request to prompt create
+//design header banner
+//set up div that will receive returned prompt value to user when generated
+//set up top level menu that you can select to see all prompt images?
