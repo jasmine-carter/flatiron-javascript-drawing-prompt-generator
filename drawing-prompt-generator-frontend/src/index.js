31bd6d99832b329dbc30e4d54ebb2d6b6d1aa479
Jasmine Carter <heyjasminec@gmail.com>
2020-12-18 13:11:21 -0500

    Add Event listener to generate prompt button click, add generate prompt function to retrieve random prompt from API

--- drawing-prompt-generator-frontend/src/index.js
+++ drawing-prompt-generator-frontend/src/index.js
@@ -3,3 +3,31 @@
 //design header banner
 //set up div that will receive returned prompt value to user when generated
 //set up top level menu that you can select to see all prompt images?
+
+let promptButton = document.querySelector(".get-prompt")
+promptButton.addEventListener("click", event=> {
+  console.log("you done clicked it!")
+  generatePrompt()
+})
+
+
+function generatePrompt() {
+  //this function will hit create prompt endpoint
+  //feed response to updateUserFacingPrompt
+  return fetch("http://localhost:3000/prompts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "test": "test value"
+    })
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object)
+  })
+}
