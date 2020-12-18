995d4e15533fdcdac49a5f962ff91fdaa94c008e
Jasmine Carter <heyjasminec@gmail.com>
2020-12-18 13:11:55 -0500

    Adjust create action to render new prompt.sentences.first value

--- drawing-prompt-generator-backend/app/controllers/prompts_controller.rb
+++ drawing-prompt-generator-backend/app/controllers/prompts_controller.rb
@@ -1,18 +1,20 @@
 class PromptsController < ApplicationController
 
+  def index
+    prompts = Prompt.all
+    render json: {test: "success!"}
+  end
 
   def create
     #if no values are provided, create random prompt
     #else create prompts based on values from user
-    binding.pry
     verb = Verb.random_verb
     noun = Noun.random_noun
     adjective = Adjective.random_adjective#find or create when user submitted
     style = Style.random_style
     prompt = Prompt.create(style: style)
-      prompt.save
-    sentence = Sentence.create(prompt: prompt, value: "A #{adjective.value} #{noun.value}, #{verb.noun} in the style of #{style.value}", noun_id: noun.id, verb_id: verb.id, adjective_id: adjective.id)
-
+    sentence = Sentence.create(prompt: prompt, value: "A #{adjective.value} #{noun.value}, #{verb.value} in the style of #{style.value}", noun_id: noun.id, verb_id: verb.id, adjective_id: adjective.id)
+    render json: prompt.sentences.first
   end
 
 
