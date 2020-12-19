5883ac2cf0fdd199bb65dbda5ba3eb8309334633
Jasmine Carter <heyjasminec@gmail.com>
2020-12-19 16:17:14 -0500

    Adjust Prompt Create controller action to account for user submitted params vs. net new Prompt

--- drawing-prompt-generator-backend/app/controllers/prompts_controller.rb
+++ drawing-prompt-generator-backend/app/controllers/prompts_controller.rb
@@ -6,15 +6,28 @@ class PromptsController < ApplicationController
   end
 
   def create
+    binding.pry
+    if params["noun"].present?
+      noun = Noun.find_or_create_by(value: params["noun"])
+      verb = Verb.find_or_create_by(value: params["verb"])
+      adjective = Adjective.find_or_create_by(value: params["adjective"])
+      style = Style.find_or_create_by(value: params["style"])
+      prompt = Prompt.create(style: style)
+        #create a class method to DRY this up and remove from controller?
+      sentence = Sentence.create(prompt: prompt, value: "A #{adjective.value} #{noun.value}, #{verb.value} in the style of #{style.value}", noun_id: noun.id, verb_id: verb.id, adjective_id: adjective.id)
+      #create a class method for prompt to solve for the below line
+      render json: prompt.sentences.first
     #if no values are provided, create random prompt
     #else create prompts based on values from user
-    verb = Verb.random_verb
-    noun = Noun.random_noun
-    adjective = Adjective.random_adjective#find or create when user submitted
-    style = Style.random_style
-    prompt = Prompt.create(style: style)
-    sentence = Sentence.create(prompt: prompt, value: "A #{adjective.value} #{noun.value}, #{verb.value} in the style of #{style.value}", noun_id: noun.id, verb_id: verb.id, adjective_id: adjective.id)
-    render json: prompt.sentences.first
+    else
+      verb = Verb.random_verb
+      noun = Noun.random_noun
+      adjective = Adjective.random_adjective#find or create when user submitted
+      style = Style.random_style
+      prompt = Prompt.create(style: style)
+      sentence = Sentence.create(prompt: prompt, value: "A #{adjective.value} #{noun.value}, #{verb.value} in the style of #{style.value}", noun_id: noun.id, verb_id: verb.id, adjective_id: adjective.id)
+      render json: prompt.sentences.first
+    end
   end
 
 
