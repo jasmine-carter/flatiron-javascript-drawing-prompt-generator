38321ddb6f397e5508d5309044faca621189b266
Jasmine Carter <heyjasminec@gmail.com>
2020-12-19 17:20:35 -0500

    Build out class method for sentences to potentially enable multiple sentence structure

--- drawing-prompt-generator-backend/app/models/sentence.rb
+++ drawing-prompt-generator-backend/app/models/sentence.rb
@@ -3,4 +3,9 @@ class Sentence < ApplicationRecord
   belongs_to :noun
   belongs_to :verb
   belongs_to :adjective
+
+  def structure(adjective, noun, verb, style)
+    self.value = "A #{adjective.value} #{noun.value}, #{verb.value} in the style of #{style.value}"
+  end
+
 end
