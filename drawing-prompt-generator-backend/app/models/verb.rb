76192fcb4e5124babb65abb392afbeb9ba78a72e
Jasmine Carter <heyjasminec@gmail.com>
2020-12-20 14:57:45 -0500

    Remove faker gem from verb model for generation

--- drawing-prompt-generator-backend/app/models/verb.rb
+++ drawing-prompt-generator-backend/app/models/verb.rb
@@ -3,8 +3,7 @@ class Verb < ApplicationRecord
   has_many :prompts, through: :sentences
 
   def self.random_verb
-    value = Faker::Verb.ing_form
-    Verb.create(value: value)
+    Verb.all.sample
   end
 
 end
